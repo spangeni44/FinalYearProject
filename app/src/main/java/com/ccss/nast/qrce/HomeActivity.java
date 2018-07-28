f7a5b6b383de50347d54b182e5cd7ef6e2aad815
Suresh Pangeni <spangeni44@gmail.com>
2018-07-28 09:34:19 +0545

    splash full screen mode

--- app/src/main/java/com/ccss/nast/qrce/HomeActivity.java
+++ app/src/main/java/com/ccss/nast/qrce/HomeActivity.java
@@ -17,5 +17,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onPause(){
         super.onPause();
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
     }
 }
